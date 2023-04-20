831eeabd64d1d4f55aacc9d11d161a611f596330
Dan Chavez <125321162+DRChavez505@users.noreply.github.com>
2023-04-20 04:35:07 -0600

    Update CapStoneDChavez.ino
    "git add ." is not working perhaps I built it incorrectly.

--- CapStoneDChavez/CapStoneDChavez/src/CapStoneDChavez.ino
+++ CapStoneDChavez/CapStoneDChavez/src/CapStoneDChavez.ino
@@ -1,28 +1,83 @@
 /*
  * Project CapStoneDChavez
- * Description: Weather Chamber
+ * Description: weather Chamber
  * Author:Dan Chavez
- * Date:4.19.2023
+ * Date: 4.19.2023
  */
-
+#include "IoTClassroom_CNM.h"
+#include <math.h>
+#include "particle.h"
+#include "Adafruit_BME280.h"
+#include "Adafruit_SSD1306.h"
+#include "Adafruit_GFX.h"
+Adafruit_SSD1306 display(D3);
+Adafruit_BME280 bme;
+int rot=2;
+bool status;
+float tempC; 
+float pressPA;
+float humidRH;
+const int D=2;
+Button testButton (D3); 
+// relay stuff
+const int relayPin=D5;
+bool RelayState;
+bool offOn=1;
+bool onOff=0;
 // setup() runs once, when the device is first turned on.
-#include "math.h"
-#include "Stepper.h"
-Stepper myStepper(2048, D8, D7, D6, D5);
+
 void setup() {
-myStepper.setSpeed(15);
-Wire.begin();
-Wire.beginTransmission(0x68);
-Wire.write(0x6B);
-Wire.write(0x00);
-Wire.endTransmission(true);
-// Put initialization like pinMode and begin functions here.
-}
-// loop() runs over and over again, as quickly as it can execute.
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setRotation(rot);
+  display.display(); 
+  delay(1000);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.display();
+  display.clearDisplay();
+  pinMode(relayPin, OUTPUT);
+  Serial.printf("Basic test:");
+  waitFor(Serial.isConnected,15000);
+  WiFi.on();
+  WiFi.setCredentials("IoTNetwork");
+  WiFi.connect();
+  while(WiFi.connecting());
+ }
+
 void loop() {
-myStepper.step(4096);
-delay(500);
-myStepper.step(-2048);
-delay(500);
+    status = bme.begin(0x76);
+    if (status == false)
+    {
+      Serial.printf("BME280 at address 0x%02X failed to start", 0x76);
+      }
+      tempC = bme.readTemperature(); //deg C
+      pressPA = bme.readPressure();  //pascals
+      humidRH = bme.readHumidity (); // %RH
+      display.printf("Temp %0.1f\nHumidity %0.1f\nPressure %f0.1", tempC, humidRH, pressPA);
+      display.display();
+      delay(1000);
+      display.display();
+  if(BUTTON1_PRESSED){
+  void turnRelayOn();
+    delay(5000);
+    Serial.printf("button is pressed\n");
+  }
+  else{
+   void turnRelayOff();
+    delay(5000);
+    Serial.printf("Button is not pressed \n");
+  }
 }
-// The core of your code will likely live here.
+  void turnRelayOn(){
+  Serial.printf("activating pump %i\n",relayPin);
+  digitalWrite(relayPin, HIGH);
+   
+   switchON(relayPin);
+   digitalWrite(relayPin, offOn);
+}
+
+void turnRelayOff()  {
+   digitalWrite(relayPin, onOff);
+   Serial.printf("turning pump off %i\n",relayPin);
+}
